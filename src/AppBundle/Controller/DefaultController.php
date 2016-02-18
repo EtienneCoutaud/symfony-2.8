44796ee5dcbc4b8e4c04dec92fc338913af275b6
Etienne Coutaud <ecoutaud@octo.com>
2016-02-18 19:47:01 +0100

    add memcache

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
 
 class DefaultController extends Controller
 {
@@ -13,9 +16,26 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+
+        $session = new Session();
+        $session->start();
+
+        // set and get session attributes
+        $session->set('name', 'BOB');
+        $retMemcache = $session->get('name');
+
+
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'), 'retMemcache' => $retMemcache,
         ));
+
+
+
+
+
+
     }
 }
